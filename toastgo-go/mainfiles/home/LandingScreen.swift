53017551e49e47aad1ac273ae951a773116bbb51
SAN <12975157+thegitparticle@users.noreply.github.com>
2021-10-15 02:04:08 +0530

    swipeable bittom tabs for landing - working

--- toastgo-go/mainfiles/home/LandingScreen.swift
+++ toastgo-go/mainfiles/home/LandingScreen.swift
@@ -7,52 +7,70 @@
 
 import SwiftUI
 
+let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+let statusBarHeight = window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+
 struct LandingScreen: View {
 	
-	@State private var buttonActive: Bool = false
+	@State var index = 0
 	
-	@State private var selectedTab = 0
-	
-	let numTabs = 2
-	let minDragTranslationForSwipe: CGFloat = 50
-	
-    var body: some View {
-			TabView(selection: $selectedTab) {
+	var body: some View {
+		ZStack(alignment: .top) {
+			
+			VStack {
 				
-				NavigationView{
-					Text("Hello, World!")
-				}.tabItem {
-					Image(systemName: "house")
-					Text("Home")
-				}.tag(0)
-				.highPriorityGesture(DragGesture().onEnded({
-					self.handleSwipe(translation: $0.translation.width)
-				}))
+				TabView(selection: $index) {
+					
+					ForEach (0..<2) { pageId in
+						VStack {
+							
+							Text("Page \(pageId)").tag(pageId)
+							Text("now, you are inside the ayeverse!").font(LightTheme.Typography.h1)
+							Text("now, you are inside the ayeverse!").font(LightTheme.Typography.h3)
+							Text("now, you are inside the ayeverse!").font(LightTheme.Typography.body2)
+							Spacer()
+							Text("now, you are inside the ayeverse!").font(LightTheme.Typography.subtitle2)
+							Text("now, you are inside the ayeverse!").font(.system(size: 13))
+							Text("now, you are inside the ayeverse!").font(LightTheme.Typography.subtitle2I)
+							Spacer()
+						}
+					}
+				}
+				.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
 				
-				NavigationView{
-					Text("Salut, tout le monde!")
-				}.tabItem {
-					Image(systemName: "timelapse")
-					Text("Space")
-				}.tag(1)
-				.highPriorityGesture(DragGesture().onEnded({
-					self.handleSwipe(translation: $0.translation.width)
-				}))
-				
-			}.navigationBarHidden(true)
-    }
-	
-	private func handleSwipe(translation: CGFloat) {
-		if translation > minDragTranslationForSwipe && selectedTab > 0 {
-			selectedTab -= 1
-		} else  if translation < -minDragTranslationForSwipe && selectedTab < numTabs-1 {
-			selectedTab += 1
-		}
+			}.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+			
+			TabBarView(index: $index)
+		}.navigationBarHidden(true).background(LightTheme.Colors.textPrimary).frame(maxWidth: .infinity, maxHeight: .infinity).edgesIgnoringSafeArea(.all)
 	}
+	
+}
+
+struct TabBarView: View {
+	@Binding var index: Int
+	var titles = ["Home", "Directs"]
+	var body: some View {
+		HStack {
+			ForEach(titles.indices) {id in
+				let title = Text(titles[id]).id(id).foregroundColor(LightTheme.Colors.uiBackground).font(LightTheme.Typography.h4).padding(20)
+					.onTapGesture {
+						withAnimation() {
+							index = id
+						}
+					}
+				if self.index == id {
+					title.foregroundColor(.black)
+				} else {
+					title.foregroundColor(.gray)
+				}
+			}
+		}.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom).padding(40)
+	}
+	private let leftOffset: CGFloat = 0.1
 }
 
 struct LandingScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        LandingScreen()
-    }
+	static var previews: some View {
+		LandingScreen()
+	}
 }
