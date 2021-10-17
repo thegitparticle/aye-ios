2bcdcfecbc40e5f793cd3a13e21245822e67f272
SAN <12975157+thegitparticle@users.noreply.github.com>
2021-10-17 16:28:43 +0530

    cam buttons setup

--- toastgo-go/mainfiles/home/talk/camera/TalkCameraScreen.swift
+++ toastgo-go/mainfiles/home/talk/camera/TalkCameraScreen.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftUICam
+import SwiftUIFontIcon
 
 struct TalkCameraScreen: View {
 	
@@ -25,40 +26,88 @@ struct TalkCameraScreen: View {
 	var my_id: Int
 	var my_name: String
 	
-    var body: some View {
+	var body: some View {
 		
 		ZStack {
-			CameraView(events: events, applicationName: "SwiftUICam")
-			CameraInterfaceView(events: events)
+			
+			CameraView(events: events, applicationName: "toastgo-go")
+			
+			VStack {
+				CameraInterfaceView(events: events)
+			}.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+			
 		}.navigationBarHidden(true).background(Color.black).frame(maxWidth: .infinity, maxHeight: .infinity).edgesIgnoringSafeArea(.all)
 		
-    }
+	}
 }
 
 struct CameraInterfaceView: View, CameraActions {
 	@ObservedObject var events: UserEvents
 	
 	var body: some View {
-		VStack {
-			HStack {
-//				rotateButton().onTapGesture {
-//					self.rotateCamera(events: events)
-//				}
-				Spacer()
-//				flashButton().onTapGesture {
-//					self.changeFlashMode(events: events)
-//				}
+		
+		HStack {
+			
+			ZStack {
+				
+				Circle().frame(width: 20, height: 20)
+					.padding()
+					.foregroundColor(Color.white)
+					.background(Color.white)
+					.cornerRadius(70)
+				
+				FontIcon.text(.ionicon(code: .ios_images), fontsize: 25).foregroundColor(Color.black)
+				
+			}.padding(.horizontal, 20)
+			
+			ZStack {
+				
+				Circle().frame(width: 35, height: 35)
+					.padding()
+					.foregroundColor(Color.white)
+					.background(Color.white)
+					.cornerRadius(70)
+				
+			}.padding(.horizontal, 20).onTapGesture {
+				self.takePhoto(events: events)
 			}
-			Spacer()
-//			captureButton().onTapGesture {
-//				self.takePhoto(events: events)
-//			}
-		}
+			
+			ZStack {
+				
+				Circle().frame(width: 20, height: 20)
+					.padding()
+					.foregroundColor(Color.white)
+					.background(Color.white)
+					.cornerRadius(70)
+				
+				FontIcon.text(.ionicon(code: .ios_reverse_camera), fontsize: 25).foregroundColor(Color.black)
+				
+			}.padding(.horizontal, 20).onTapGesture {
+				self.rotateCamera(events: events)
+			}
+			
+		}.padding(.vertical, 30)
+		
+		//		VStack {
+		//			HStack {
+		////				rotateButton().onTapGesture {
+		////					self.rotateCamera(events: events)
+		////				}
+		//				Spacer()
+		////				flashButton().onTapGesture {
+		////					self.changeFlashMode(events: events)
+		////				}
+		//			}
+		//			Spacer()
+		////			captureButton().onTapGesture {
+		////				self.takePhoto(events: events)
+		////			}
+		//		}
 	}
 }
 
 struct TalkCameraScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        TalkCameraScreen(clubName: "", clubId: 0, channelId: "", ongoingFrame: false, startTime: "", endTime: "", ongoingStream: false, ongoingStreamUser: "", directornot: false, my_id: 82, my_name: "San")
-    }
+	static var previews: some View {
+		TalkCameraScreen(clubName: "", clubId: 0, channelId: "", ongoingFrame: false, startTime: "", endTime: "", ongoingStream: false, ongoingStreamUser: "", directornot: false, my_id: 82, my_name: "San")
+	}
 }
