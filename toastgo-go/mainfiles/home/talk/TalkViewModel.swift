19ca2ed03e3839a99fdcb0b2d58997f6a42c833f
Set <12975157+thegitparticle@users.noreply.github.com>
2021-10-19 20:04:27 +0530

    pubnub config context for all app try 2

--- toastgo-go/mainfiles/home/talk/TalkViewModel.swift
+++ toastgo-go/mainfiles/home/talk/TalkViewModel.swift
@@ -174,19 +174,19 @@ class TalkViewModel: ObservableObject {
 	}
 	
 	
-	public func subscribeToPnChannel (channelId: String) {
-		
-		let config = PubNubConfiguration(
-			publishKey: "pub-c-a65bb691-5b8a-4c4b-aef5-e2a26677122d",
-			subscribeKey: "sub-c-d099e214-9bcf-11eb-9adf-f2e9c1644994",
-			uuid: String(UserDefaults.standard.integer(forKey: "MyId"))
-		)
-		
-		let pubnub = PubNub(configuration: config)
-		
-		pubnub.subscribe(to: [channelId])
-		
-		
-	}
+//	public func subscribeToPnChannel (channelId: String) {
+//
+//		let config = PubNubConfiguration(
+//			publishKey: "pub-c-a65bb691-5b8a-4c4b-aef5-e2a26677122d",
+//			subscribeKey: "sub-c-d099e214-9bcf-11eb-9adf-f2e9c1644994",
+//			uuid: String(UserDefaults.standard.integer(forKey: "MyId"))
+//		)
+//
+//		let pubnub = PubNub(configuration: config)
+//
+//		pubnub.subscribe(to: [channelId])
+//
+//
+//	}
 	
 }
