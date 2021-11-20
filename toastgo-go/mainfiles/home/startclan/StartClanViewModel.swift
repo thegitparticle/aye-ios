9556e05e54dca2596f2bad8d0a01641540b46697
Set <12975157+thegitparticle@users.noreply.github.com>
2021-11-20 18:00:29 +0530

    start clan related apis in vm

--- toastgo-go/mainfiles/home/startclan/StartClanViewModel.swift
+++ toastgo-go/mainfiles/home/startclan/StartClanViewModel.swift
@@ -12,6 +12,8 @@ class StartClanViewModel: ObservableObject {
 	@Published var friendsList = [FriendsListItemDataClass]()
 	@Published var contactsList = [ContactsListItemDataClass]()
 	
+	@Published var newClanDetailsFromServer = StartClanResponseDataClass(id: 0, name: "", members: "", admin_leader: 0, pn_channel_id: "")
+	
 	init () {
 		
 		getFriendsListHere()
@@ -71,6 +73,124 @@ class StartClanViewModel: ObservableObject {
 				}
 				
 				print("debuglogs Fetch failed contacts list: \(error?.localizedDescription ?? "Unknown error")")
+			}
+			
+		}.resume()
+	}
+	
+	public func postStartNewClan(clan_name: String) {
+		
+		var payload = ["name": clan_name , "members": String(UserDefaults.standard.integer(forKey: "MyId")), "admin_leader": UserDefaults.standard.integer(forKey: "MyId")] as [AnyHashable : Any]
+		
+		guard let url = URL(string: "https://apisayepirates.life/api/clubs/create_club/") else {
+			return
+		}
+		
+		var request = URLRequest(url: url)
+		
+		request.httpMethod = "POST"
+		
+		do {
+			request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: [])
+		} catch let error {
+			print(error.localizedDescription)
+		}
+		
+		
+		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+		
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			
+			if let data = data {
+				
+				if let decodedResponse = try? JSONDecoder().decode(StartClanResponseDataClass.self, from: data) {
+					
+					DispatchQueue.main.async {
+						
+						self.newClanDetailsFromServer = decodedResponse
+					}
+				}
+			}
+		}.resume()
+	}
+	
+	public func getAddFriendsToClan(added_user: Int, clan_id: Int) {
+		
+		let userid = UserDefaults.standard.integer(forKey: "MyId")
+		
+		guard let url = URL(string: "https://apisayepirates.life/api/users/add_users_to_club/\(String(added_user))/\(String(clan_id))/") else {
+			return
+		}
+		
+		let request = URLRequest(url: url)
+		
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			
+			if let data = data {
+				
+				if let decodedResponse = try? JSONDecoder().decode([ContactsListItemDataClass].self, from: data) {
+					
+					DispatchQueue.main.async {
+						
+					}
+					
+					return
+				}
+				
+//				print("debuglogs Fetch failed contacts list: \(error?.localizedDescription ?? "Unknown error")")
+			}
+			
+		}.resume()
+	}
+	
+	public func getInviteContactsToClan(invited_phone: String, clan_id: Int) {
+		
+		let userid = UserDefaults.standard.integer(forKey: "MyId")
+		
+		guard let url = URL(string: "https://apisayepirates.life/api/users/send_invite/\(invited_phone)/\(String(userid))/") else {
+			return
+		}
+		
+		let request = URLRequest(url: url)
+		
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			
+			if let data = data {
+				
+				if let decodedResponse = try? JSONDecoder().decode([ContactsListItemDataClass].self, from: data) {
+					
+					DispatchQueue.main.async {
+						
+					}
+					
+					return
+				}
+				
+				//				print("debuglogs Fetch failed contacts list: \(error?.localizedDescription ?? "Unknown error")")
+			}
+			
+		}.resume()
+		
+		guard let url2 = URL(string: "https://apisayepirates.life/api/users/add_invited_user/\(invited_phone)/\(String(userid))/\(String(clan_id))/") else {
+			return
+		}
+		
+		let request2 = URLRequest(url: url2)
+		
+		URLSession.shared.dataTask(with: request2) { data, response, error in
+			
+			if let data = data {
+				
+				if let decodedResponse = try? JSONDecoder().decode([ContactsListItemDataClass].self, from: data) {
+					
+					DispatchQueue.main.async {
+						
+					}
+					
+					return
+				}
+				
+				//				print("debuglogs Fetch failed contacts list: \(error?.localizedDescription ?? "Unknown error")")
 			}
 			
 		}.resume()
