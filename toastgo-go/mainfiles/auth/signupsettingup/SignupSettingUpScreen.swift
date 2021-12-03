6cdaa60918ae8cd67cfbed6cd7552a950d8d51a7
Set <12975157+thegitparticle@users.noreply.github.com>
2021-12-03 12:02:41 +0530

    sign up setting up - contacts upload - screen & vm setup

---
+++ toastgo-go/mainfiles/auth/signupsettingup/SignupSettingUpScreen.swift
@@ -0,0 +1,169 @@
+//
+//  SignupSettingUpScreen.swift
+//  toastgo-go
+//
+//  Created by SAN on 12/3/21.
+//
+
+import SwiftUI
+import Contacts
+import CoreLocation
+
+struct SignupSettingUpScreen: View {
+	@StateObject private var viewModel = SignupSettingUpViewModel()
+	
+	var phoneNumber: String
+	var countryCode: String
+	var userDeets: UserDetailsDataClass
+	
+	var permissionGiven: Bool = true
+	
+	@State private var contacts = []
+	
+	@State private var contactsButtonActive: Bool = false
+	@State private var showingSettingsGoingAlert = false
+	
+	@State private var activateLandingScreenNav = false
+	
+	var body: some View {
+		ZStack(alignment: .leading) {
+			
+			NavigationLink(destination: LandingScreen(), isActive: self.$activateLandingScreenNav) {EmptyView()}
+			
+			VStack() {
+				
+				Spacer()
+				
+				HStack(alignment: .center) {
+					if (contactsButtonActive) {
+						LottieView(filename: "contacts_lottie")
+					} else {
+						LottieView(filename: "loading_ping_pong_cup")
+					}
+				}.padding(20).frame(maxWidth: .infinity, maxHeight: 200)
+				
+				if (contactsButtonActive) {
+					Text("sup? needs contacts to work").foregroundColor(LightTheme.Colors.uiBackground).font(LightTheme.Typography.h5).padding(20)
+				} else {
+					Text("setting up your profile!").foregroundColor(LightTheme.Colors.uiBackground).font(LightTheme.Typography.h5).padding(20)
+				}
+				
+				Spacer().onAppear() {
+					self.requestAccess()
+					self.getContacts()
+				}
+				
+				if (contactsButtonActive) {
+					Button(action: {
+						//						UserDefaults.standard.set(true, forKey: "LoginState");
+						contactsButtonActive = true
+						self.sendToSettingsForPermissions()
+					}) {
+						Text("allow contacts in settings")
+							.frame(minWidth: 0, maxWidth: 300, minHeight: 0, maxHeight: 40)
+							.padding()
+							.font(LightTheme.Typography.h5)
+							.foregroundColor(LightTheme.Colors.uiSurface)
+							.background(LightTheme.Colors.textSecondary)
+							.cornerRadius(50)
+					}
+					.padding(.vertical).padding(20)
+				}
+				
+			}.frame(maxWidth: .infinity, maxHeight: .infinity).onChange(of: viewModel.contactsSycned, perform: {syncStatus in onClickNextButton(status: syncStatus)})
+			
+		}.navigationBarHidden(true).background(LightTheme.Colors.textPrimary).frame(maxWidth: .infinity, maxHeight: .infinity).edgesIgnoringSafeArea(.all)
+	}
+	
+	func onClickNextButton (status: String) {
+		if (status == "Worked") {
+			
+			UserDefaults.standard.set(userDeets.image, forKey: "MyDp");
+			UserDefaults.standard.set(userDeets.user.full_name, forKey: "MyName");
+			UserDefaults.standard.set(userDeets.user.id, forKey: "MyId");
+			UserDefaults.standard.set(userDeets.user.phone, forKey: "MyPhone");
+			
+			UserDefaults.standard.set(true, forKey: "LoginState");
+			
+			self.activateLandingScreenNav = true
+			
+		} else if (status == "No") {
+			print("debuglogs code", "contacts upload error")
+			self.requestAccess()
+			self.getContacts()
+		} else {
+			
+		}
+	}
+	
+	func requestAccess() {
+		let store = CNContactStore()
+		switch CNContactStore.authorizationStatus(for: .contacts) {
+		case .authorized:
+			self.getContacts()
+			self.contactsButtonActive = false
+		case .denied:
+			store.requestAccess(for: .contacts) { granted, error in
+				if granted {
+					self.getContacts()
+					self.contactsButtonActive = false
+				} else {
+					self.contactsButtonActive = true
+				}
+			}
+		case .restricted, .notDetermined:
+			store.requestAccess(for: .contacts) { granted, error in
+				if granted {
+					self.getContacts()
+				} else {
+					self.contactsButtonActive = true
+				}
+			}
+		@unknown default:
+			print("error")
+		}
+	}
+	
+	func getContacts() {
+		DispatchQueue.main.async {
+			self.contacts = FetchContactsWhileSignup().fetchingContacts()
+			print("fetched contacts function done")
+			if (self.contacts.count > 2) {
+				viewModel.postOtpToServerSignup(userid: String(self.userDeets.user.id), countryCode: self.countryCode, contactsList: self.contacts)
+			}
+			print(self.$contacts)
+		}
+	}
+	
+	func sendToSettingsForPermissions() {
+		
+		UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+	}
+	
+}
+
+
+class FetchContactsWhileSignup {
+	
+	func fetchingContacts() -> [Any]{
+		var contacts = [Any]()
+		let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey]
+		let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
+		
+		print("fetched contacts fucntion called and request is setup")
+		
+		do {
+			try CNContactStore().enumerateContacts(with: request, usingBlock: { (contact, stopPointer) in
+				contacts.append(["name": contact.givenName, "phone": contact.phoneNumbers.first?.value.stringValue ?? ""])
+			})
+			print("fetched contacts success")
+		} catch let error {
+			print("Failed", error)
+		}
+		
+		print(contacts)
+		return contacts
+		
+	}
+}
+
---
+++ toastgo-go/mainfiles/auth/signupsettingup/SignupSettingUpViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  SignupSettingUpViewModel.swift
+//  toastgo-go
+//
+//  Created by SAN on 12/3/21.
+//
+
+import Foundation
+
+class SignupSettingUpViewModel: ObservableObject {
+	
+	@Published var contactsSycned: String = ""
+	
+	public func postOtpToServerSignup(userid: String, countryCode: String, contactsList: [Any]) {
+		
+		print("post otp to server func called")
+		
+		print("passed contacts", contactsList)
+		
+		var payload_2: [Any] = contactsList
+		
+		payload_2.insert(contentsOf: [["country_code", countryCode]], at: 0)
+		
+		var jsonHere: Data = Data()
+		
+		do {
+			jsonHere = try JSONSerialization.data(withJSONObject: payload_2, options: [])
+		} catch let error {
+			print(error.localizedDescription)
+		}
+		
+		var JSONString: String = ""
+		
+		JSONString = String(data: jsonHere, encoding: String.Encoding.utf8) ?? ""
+		
+		let payload = ["contact_list": JSONString]
+		
+		print("paylod", payload)
+		
+		guard let url = URL(string: "https://apisayepirates.life/api/users/post_contacts_to_server/\(userid)/") else {
+			return
+		}
+		
+		var request = URLRequest(url: url)
+		
+		request.httpMethod = "PUT"
+		
+		do {
+			request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: [])
+		} catch let error {
+			print(error.localizedDescription)
+		}
+		
+		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+		
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			
+			if let data = data {
+				DispatchQueue.main.async {
+					self.contactsSycned = "Worked"
+				}
+			}
+			print("debuglogs Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+		}.resume()
+	}
+	
+}
